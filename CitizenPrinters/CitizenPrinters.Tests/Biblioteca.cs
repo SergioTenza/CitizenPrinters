15dbaa93244bc77e8566951f78b960fd7ed47614
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2021-10-30 09:17:30 +0200

    INFRA: Added Projects

---
+++ CitizenPrinters/CitizenPrinters.Tests/Biblioteca.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace CitizenPrinters.Tests
+{
+    public class Biblioteca
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
