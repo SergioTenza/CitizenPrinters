7d6ca1370553144285cda715d6347c9422e833a1
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2021-12-27 22:21:27 +0100

    TEST: ShouldCreateClassForCX02WPrinterAndNotThrowException -> FAIL

--- CitizenPrinters/CitizenPrinters.Tests/CitizenPrinters.Core.Vb.x86.Tests/CitizenPrintersCoreTest.cs
+++ CitizenPrinters/CitizenPrinters.Tests/CitizenPrinters.Core.Vb.x86.Tests/CitizenPrintersCoreTest.cs
@@ -6,7 +6,7 @@ using System;
 
 namespace CitizenPrinters.Tests.CitizenPrinters.Core.Vb.x86
 {
-    public class CitizenPrintersCoreVbx86Test
+    public class CitizenPrintersCoreTest
     {
         private CitizenPrinter citizenPrinter;
         
@@ -75,6 +75,37 @@ namespace CitizenPrinters.Tests.CitizenPrinters.Core.Vb.x86
             else if (IntPtr.Size == 4)
             {
                 Assert.IsInstanceOf<CX02_x86>(citizenPrinter.Printer);
+            }
+
+        }
+
+        [Test]
+        public void ShouldCreateClassForCX02WPrinterAndNotThrowException()
+        {
+
+            /*
+            * What: Try to instantiate a new Printer Class
+            * Where: On Main Form StartUp
+            * When: A printer is selected on Combobox
+            */
+
+            //Given
+            string name = "CITIZEN CX-02W";
+
+
+            //When
+            Assert.DoesNotThrow(() => citizenPrinter = new CitizenPrinter(name));
+
+
+            //Then
+            Assert.IsNotNull(citizenPrinter);
+            if (IntPtr.Size == 8)
+            {
+                Assert.IsInstanceOf<CX02W_x64>(citizenPrinter.Printer);
+            }
+            else if (IntPtr.Size == 4)
+            {
+                Assert.IsInstanceOf<CX02W_x86>(citizenPrinter.Printer);
             }
 
         }
