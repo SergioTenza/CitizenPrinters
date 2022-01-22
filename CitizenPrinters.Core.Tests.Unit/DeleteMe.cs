f950d142a5dd6e06e7fad548640720fd8c98a72d
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2022-01-22 18:24:33 +0100

    INFRA: Added XUnit Project

---
+++ CitizenPrinters.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace CitizenPrinters.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void DeleteThis()
+        {
+            Assert.True(true);
+        }
+    }
+}
