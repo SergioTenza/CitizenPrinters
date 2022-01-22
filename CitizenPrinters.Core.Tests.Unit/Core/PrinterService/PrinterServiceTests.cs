a8453983a6af0bb0295b7d8b0813d8506c4d1d45
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2022-01-22 18:50:50 +0100

    PrinterServiceTest ShouldCreateClassForCyPrinterAndNotThrowException -> PASS

---
+++ CitizenPrinters.Core.Tests.Unit/Core/PrinterService/PrinterServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using CitizenPrinters.Core.Models.CitizenPrinters;
+using CitizenPrinters.Core.Models.Printers;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CitizenPrinters.Core.Tests.Unit.Core.PrinterService
+{
+    public class PrinterServiceTests
+    {
+        [Theory]
+        [InlineData("CITIZEN CX-02")]
+        [InlineData("CITIZEN CX-02W")]
+        [InlineData("CY")]
+        public void ShouldCreateClassForCyPrinterAndNotThrowException(
+            string printerName)
+        {
+
+            /*
+            * What:
+            * Where:
+            * When:
+            */
+
+            //Given
+            CitizenPrinter citizenPrinter = new CitizenPrinter(printerName);
+            var type = citizenPrinter.Printer.GetType();
+            //When
+
+
+            //Then
+            Assert.NotNull(citizenPrinter.Printer);
+            Assert.True(IsSameTypeAs(citizenPrinter.Printer));
+            
+        }
+
+        private bool IsSameTypeAs(Printer printer)
+        {
+            return printer.GetType().BaseType == typeof(Printer);
+             
+        }
+    }
+}
