2558dc102b368e7ad83699f90520731dd025a728
Sergio Tenza Lopez <sergeix_tc@hotmail.com>
2022-01-22 18:59:26 +0100

    ShouldCreateClassForCyPrinterAndNotThrowException -> PASS

--- CitizenPrinters.Core.Tests.Unit/Core/PrinterService/PrinterServiceTests.cs
+++ CitizenPrinters.Core.Tests.Unit/Core/PrinterService/PrinterServiceTests.cs
@@ -31,13 +31,25 @@ namespace CitizenPrinters.Core.Tests.Unit.Core.PrinterService
             //Then
             Assert.NotNull(citizenPrinter.Printer);
             Assert.True(IsSameTypeAs(citizenPrinter.Printer));
+            Assert.True(IsInstanceOf(citizenPrinter.Printer));
             
         }
 
         private bool IsSameTypeAs(Printer printer)
         {
             return printer.GetType().BaseType == typeof(Printer);
-             
+        }
+
+        private bool IsInstanceOf(Printer printer) 
+        { 
+            return IntPtr.Size == 8 ? 
+                printer.GetType() == typeof(CY_x64) ||
+                printer.GetType() == typeof(CX02_x64) ||
+                printer.GetType() == typeof(CX02W_x64) 
+                :
+                printer.GetType() == typeof(CY_x86) ||
+                printer.GetType() == typeof(CX02_x86) ||
+                printer.GetType() == typeof(CX02W_x86);
         }
     }
 }
